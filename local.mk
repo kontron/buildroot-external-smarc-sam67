0d10e2e2e0c31d971b9442adbd03be9ca2cf5769
Michael Walle <michael.walle@kontron.com>
2025-02-06 09:24:57 +0100

    switch to fixed versions
    Buildroot doesn't support branch names for the versions. Switch to fixed
    versions and add a mechanism to automatically set the version of a
    package to the latest HEAD of a given branch. This only works if the
    package fetches its source from a git repository.
    
    Set USE_HEAD_VERSION to "y" to build the lastest development versions of
    linux and uboot:
      USE_HEAD_VERSION=y make
    
    Co-developed-by: Heiko Thiery <heiko.thiery@kontron.com>
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ local.mk
@@ -0,0 +1,15 @@
+define set-override-version-inner # PKG branch
+	$(eval override $(1)_VERSION := $(shell git ls-remote $(2) refs/heads/$(3) | awk '{print $$1}'))
+endef
+
+set-override-version = $(call set-override-version-inner,$(call UPPERCASE,$(1)),$(2),$(3))
+
+# Buildroot isn't able to automatically fetch the latest version. To ease
+# development and CI setups, fetch the latest version on a given branch
+# directly from the repository and override the package version with the
+# commit id.
+ifneq ($(USE_HEAD_VERSION),)
+$(call set-override-version,uboot,https://github.com/kontron/u-boot-smarc-sam67.git,master)
+$(call set-override-version,linux,https://github.com/kontron/linux-smarc-sam67.git,master)
+$(call set-override-version,ti_k3_r5_loader,https://github.com/kontron/u-boot-smarc-sam67.git,master)
+endif
