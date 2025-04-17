410186336f88dceabd8d3feb84f44fc1b3665221
Heiko Thiery <heiko.thiery@kontron.com>
2025-04-17 11:54:46 +0200

    package/k3conf: bump to latest in master
    And fixes some utils/check-package warnings.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- package/k3conf/k3conf.mk
+++ package/k3conf/k3conf.mk
@@ -4,12 +4,11 @@
 #
 ################################################################################
 
-K3CONF_VERSION = 0.3
+K3CONF_VERSION = 30a1d5b2d08c0da39d4e62b3ebee591a9882fa21
 K3CONF_SITE = https://git.ti.com/cgit/k3conf/k3conf/snapshot
 K3CONF_SOURCE = k3conf-$(K3CONF_VERSION).tar.xz
 K3CONF_LICENSE = BSD-3-Clause
 K3CONF_LICENSE_FILES = LICENSE
-K3CONF_INSTALL_STAGING = NO
 K3CONF_SUPPORTS_IN_SOURCE_BUILD = NO
 
 $(eval $(cmake-package))
