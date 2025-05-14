16275ea3cb12af4e6c76eb0a50c1e5a78d040adc
Heiko Thiery <heiko.thiery@kontron.com>
2025-05-14 16:55:25 +0200

    Add option to skip updating linux package version to HEAD
    Use env variable SKIP_LINUX_HEAD_VERSION to skip updating the version.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- local.mk
+++ local.mk
@@ -10,6 +10,8 @@ set-override-version = $(call set-override-version-inner,$(call UPPERCASE,$(1)),
 # commit id.
 ifneq ($(USE_HEAD_VERSION),)
 $(call set-override-version,uboot,https://github.com/kontron/u-boot-smarc-sam67.git,master)
-$(call set-override-version,linux,https://github.com/kontron/linux-smarc-sam67.git,master)
 $(call set-override-version,ti_k3_r5_loader,https://github.com/kontron/u-boot-smarc-sam67.git,master)
+ifeq ($(SKIP_LINUX_HEAD_VERSION),)
+$(call set-override-version,linux,https://github.com/kontron/linux-smarc-sam67.git,master)
+endif
 endif
