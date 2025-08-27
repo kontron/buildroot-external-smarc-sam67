3c7b64d1580f6db274b067d1d646d074377dcd89
Heiko Thiery <heiko.thiery@kontron.com>
2025-08-27 15:24:11 +0200

    package/mspm0flash: add new package
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

---
+++ package/mspm0flash/mspm0flash.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# mspm0flash
+#
+################################################################################
+
+MSPM0FLASH_VERSION = e37ad3cc9a063fe8ad0097a1fc54c8b83eb5fac9
+MSPM0FLASH_SITE = https://github.com/kontron/mspm0flash
+MSPM0FLASH_SITE_METHOD = git
+MSPM0FLASH_LICENSE = BSD-2-Clause
+MSPM0FLASH_LICENSE_FILES = COPYING
+MSPM0FLASH_REDISTRIBUTE = NO
+
+define MSPM0FLASH_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define MSPM0FLASH_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) DESTDIR=$(TARGET_DIR) install
+endef
+
+$(eval $(generic-package))
