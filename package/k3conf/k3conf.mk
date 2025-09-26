9386a3708725b733536c2dadb4be8e0c7a229362
Heiko Thiery <heiko.thiery@kontron.com>
2025-09-26 13:26:31 +0200

    package/k3conf: bump to version f63ccd27cba9550de04e8dbb04bc6003d8b5168e
    Signed-off-by: Heiko Thiery <heiko.thiery@kontron.com>

--- package/k3conf/k3conf.mk
+++ package/k3conf/k3conf.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-K3CONF_VERSION = e6abc1b87f1d5bc4c2ab5f1ebfbf5a70e437dcb4
-K3CONF_SITE = https://git.ti.com/cgit/k3conf/k3conf/snapshot
-K3CONF_SOURCE = k3conf-$(K3CONF_VERSION).tar.xz
+K3CONF_VERSION = f63ccd27cba9550de04e8dbb04bc6003d8b5168e
+K3CONF_SITE = https://git.ti.com/cgit/k3conf/k3conf
+K3CONF_SITE_METHOD = git
 K3CONF_LICENSE = BSD-3-Clause
 K3CONF_LICENSE_FILES = LICENSE
 K3CONF_SUPPORTS_IN_SOURCE_BUILD = NO
